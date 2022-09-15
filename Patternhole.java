754ca0c770b3b99fc0388b7804b4493de273553b
Bunny <56792505+aye-nyeinSan@users.noreply.github.com>
2022-09-15 21:28:32 +0700

    Rectangle PatternHole with Java
    Using nested for loop in Java

---
+++ Patternhole.java
@@ -0,0 +1,22 @@
+/**
+ * patternHole for interview question
+ */
+public class Patternhole {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= 5; j++) {
+                if ((i == 2 && j == 1) || (i == 3 && j == 1) || (i == 4 && j == 1)) {
+                    System.out.print("*   *");
+                    break;
+                } else {
+                    System.out.print("*");
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+
+}
